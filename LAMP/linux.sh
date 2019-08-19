baa26ad5f38cbc55d8c4fc6302d9e522febcc490
nerdeveloper <odirionye@gmail.com>
2019-08-19 13:26:44 +0100

    refactor apache2

--- LAMP/linux.sh
+++ LAMP/linux.sh
@@ -9,12 +9,11 @@ echo "***** Installing Apache, MySQL and PHP for Debiana and Ubuntu OS ********"
 
 echo "Checking for Apache Server"
 
-if [ $(/etc/init.d/apache2 status | grep -v grep | grep 'Apache2 is running' | wc -l) > 0 ]
-then
- echo -e "${PURPLE}Waoh! Apache is Installed!"
-else
-   echo "Installing Apache..."
-  sudo apt-get update && sudo apt-get install apache2 -y
-  echo -e "${PURPLE}Apache is Installed!"
-
+ps cax | grep apache2
+if [ $? -eq 1 ]
+ then
+ echo "Process is running."
+else if [ $? -eq 0 ]
+ echo "Process is not running."
+fi
 fi

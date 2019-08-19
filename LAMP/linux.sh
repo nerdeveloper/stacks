9d201cce7d47085f8a81336266d84dc515fba529
nerdeveloper <odirionye@gmail.com>
2019-08-19 14:42:35 +0100

    release linux script

--- LAMP/linux.sh
+++ LAMP/linux.sh
@@ -1,19 +1,58 @@
 #!/bin/bash
 
 RED='\033[0;31m'
-BLUE='\0;34'
+BLUE='\033[\0;34m\n'
 PURPLE='\033[0;35m'
 
 echo "***** Installing Apache, MySQL and PHP for Debiana and Ubuntu OS ********"
+sleep 1
 
+echo "***** Updating services ****"
+sudo apt-get update
+sleep 1
 
-echo "Checking for Apache Server"
-
-ps cax | grep apache2
-if [ $? -eq 1 ]
- then
- echo "Process is running."
-else if [ $? -eq 0 ]
- echo "Process is not running."
+echo  "Checking for Apache Server"
+sleep 2
+if [ $(find /etc/init.d -name apache2) ]
+then
+    echo "${BLUE} You have apache2 server installed!"
+else
+    echo -e "${RED}Installing Apache2 Server"
+    sleep 2
+    sudo apt-get update && sudo apt-get install apache2 -y
+    echo -e  "${BLUE}Apache server has been installed!"
+    
 fi
+
+echo  "Checking for MySQL Server"
+sleep 2
+if [ $(find /etc/init.d -name mysql) ]
+then
+    echo "${RED} You have MySQL server installed!"
+else
+    echo -e "${BLUE}Installing MySQL Server"
+    sleep 2
+    sudo apt install mysql-server -y 
+    echo -e  "${BLUE}MySQL server has been installed!"
+    
 fi
+
+echo  "Checking for PHP"
+sleep 2
+if [ $(find /etc -name php) ]
+then
+    echo "${RED} You have PHP installed!"
+else
+    echo -e "${BLUE}Installing PHP"
+    sleep 2
+    sudo apt install php libapache2-mod-php php-mysql
+    echo -e  "${BLUE}PHP has been installed!"
+    
+fi
+
+echo Done
+echo -e "${PURPLE}Tweet me @_nerdeveloper"
+
+
+
+

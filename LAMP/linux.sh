b5c380467426fa3dabfb2cf34241d0fad6e7c254
nerdeveloper <odirionye@gmail.com>
2019-08-19 15:16:56 +0100

    clean changes

--- LAMP/linux.sh
+++ LAMP/linux.sh
@@ -17,12 +17,12 @@ echo -e  "Checking for Apache Server"
 sleep 2
 if [ $(find /etc/init.d -name apache2) ]
 then
-    echo "${RED}You have apache2 server installed!"
+    echo -e " ${RED} You have apache2 server installed!"
 else
-    echo -e "${BLUE}Installing Apache2 Server"
+    echo -e " ${BLUE} Installing Apache2 Server"
     sleep 2
     sudo apt-get update && sudo apt-get install apache2 -y
-    echo -e  "${BLUE}Apache server has been installed!"
+    echo -e  " ${BLUE} Apache server has been installed!"
     
 fi
 
@@ -30,9 +30,9 @@ echo  "Checking for MySQL Server"
 sleep 2
 if [ $(find /etc/init.d -name mysql) ]
 then
-    echo "${RED}You have MySQL server installed!"
+    echo " ${RED}You have MySQL server installed!"
 else
-    echo -e "${BLUE} Installing MySQL Server"
+    echo -e " ${BLUE}Installing MySQL Server"
     sleep 2
     sudo apt install mysql-server -y
     echo -e  "${BLUE}MySQL server has been installed!"
@@ -43,12 +43,12 @@ echo  "Checking for PHP"
 sleep 2
 if [ $(sudo find /etc/php -name php) ]
 then
-    echo "  ${RED}You have PHP installed!"
+    echo " ${RED}You have PHP installed!"
 else
-    echo -e "  ${BLUE}Installing PHP"
+    echo -e " ${BLUE}Installing PHP"
     sleep 2
     sudo apt install php libapache2-mod-php php-mysql
-    echo -e  " ${BLUE} PHP has been installed!"
+    echo -e  "${BLUE} PHP has been installed!"
     
 fi
 

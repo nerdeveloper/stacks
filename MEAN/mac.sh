a858df63e91a98be823a487a9b75ef746702320e
nerdeveloper <odirionye@gmail.com>
2019-08-21 02:04:58 +0100

    release MEAN for mac OSX users

---
+++ MEAN/mac.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#colors
+RED='\033[0;31m'
+BLUE='\033[40;38;5;82m'
+PURPLE='\033[0;35m'
+
+#version
+NODEJS_VERSION='10'
+MONGODB_VERSION='3.6'
+
+
+
+
+echo "***** Installing MongoDB, Angular and NodeJS ********"
+sleep 2
+
+
+echo -e  "Checking for Nodejs"
+sleep 2
+
+if [ -x "$(command -v node)" ];
+then
+    echo -e " ${RED} You have NodeJS installed! \e[0m"
+else
+    echo -e " ${BLUE} Installing Nodejs and npm "
+    sleep 2
+    brew install node@${NODEJS_VERSION}
+    
+fi
+
+echo  "Checking for Express Generator"
+sleep 2
+
+if [ -x "$(command -v express-generator)" ];
+then
+    echo -e  " ${RED} You have express-generator installed! \e[0m"
+else
+    echo -e " ${BLUE} Installing express-generator"
+    sleep 2
+    npm install -g express-generator
+    echo -e  " ${BLUE} Express-generator has been installed! \e[0m"
+    
+fi
+
+
+
+echo  "Checking for Angular"
+sleep 2
+
+if [ -x "$(command -v ng)" ];
+then
+    echo -e  " ${RED} You have Angular installed! \e[0m"
+else
+    echo -e " ${BLUE} Installing Angular"
+    sleep 2
+    npm install -g @angular/cli
+    echo -e  " ${BLUE} Angular has been installed! \e[0m"
+    
+fi
+
+echo  "Checking for MongoDB"
+sleep 2
+if [ -x "$(command -v mongo)" ];
+then
+    echo -e " ${RED} You have MongoDB database Installed! \e[0m"
+else
+    echo -e " ${BLUE} Installing MongoDB "
+    sleep 2
+    brew tap mongodb/brew && \
+    brew install mongodb-community@${MONGODB_VERSION} && \
+    brew services start mongodb-community
+    echo -e  " ${BLUE} MongoDB has been installed! \e[0m "
+    
+fi
+
+echo Done
+echo -e " ${BLUE} Tweet me @_nerdeveloper \e[0m"
+
+
+
+

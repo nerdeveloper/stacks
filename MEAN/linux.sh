b848dfab00a5f6f1cc992feaf3173ce637cd5d66
nerdeveloper <odirionye@gmail.com>
2019-08-21 01:50:56 +0100

    add express-generator

--- MEAN/linux.sh
+++ MEAN/linux.sh
@@ -28,10 +28,25 @@ else
     sleep 2
     curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION} -o nodesource_setup.sh && \
     sudo bash nodesource_setup.sh && \
-    sudo apt-get install nodejs -y 
+    sudo apt-get install nodejs -y
     echo -e  " ${BLUE} Nodejs and npm has been installed! \e[0m"
     
 fi
+
+echo  "Checking for Express Generator"
+sleep 2
+if [ -x "$(command -v express-generator)" ];
+then
+    echo -e  " ${RED} You have express-generator installed! \e[0m"
+else
+    echo -e " ${BLUE} Installing express-generator"
+    sleep 2
+    sudo npm install -g express-generator
+    echo -e  " ${BLUE} Express-generator has been installed! \e[0m"
+    
+fi
+
+
 
 echo  "Checking for Angular"
 sleep 2

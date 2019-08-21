72152a545c1503ee71ca9875c803f7041914aaa1
nerdeveloper <odirionye@gmail.com>
2019-08-21 02:52:06 +0100

    remove linux bash color syntax

--- MERN/mac.sh
+++ MERN/mac.sh
@@ -21,11 +21,12 @@ sleep 2
 
 if [ -x "$(command -v node)" ];
 then
-    echo -e " ${RED} You have NodeJS installed! \e[0m"
+    echo -e " ${RED} You have NodeJS installed!"
 else
     echo -e " ${BLUE} Installing Nodejs and npm "
     sleep 2
     brew install node@${NODEJS_VERSION}
+    echo -e  " ${BLUE} Nodejs and npm has been installed!"
     
 fi
 
@@ -34,12 +35,12 @@ sleep 2
 
 if [ -x "$(command -v create-next-app)" ];
 then
-    echo -e  " ${RED} You have Create-Next-app(React) installed! \e[0m"
+    echo -e  " ${RED} You have Create-Next-app(React) installed! "
 else
-    echo -e " ${BLUE} Installing Angular"
+    echo -e " ${BLUE} Installing Create-Next-app(React)"
     sleep 2
     npm install -g create-next-app
-    echo -e  " ${BLUE} Create-Next-app(React) has been installed! \e[0m"
+    echo -e  " ${BLUE} Create-Next-app(React) has been installed!"
     
 fi
 
@@ -47,19 +48,19 @@ echo  "Checking for MongoDB"
 sleep 2
 if [ -x "$(command -v mongo)" ];
 then
-    echo -e " ${RED} You have MongoDB database Installed! \e[0m"
+    echo -e " ${RED} You have MongoDB database Installed!"
 else
     echo -e " ${BLUE} Installing MongoDB "
     sleep 2
     brew tap mongodb/brew && \
     brew install mongodb-community@${MONGODB_VERSION} && \
     brew services start mongodb-community
-    echo -e  " ${BLUE} MongoDB has been installed! \e[0m "
+    echo -e  " ${BLUE} MongoDB has been installed!"
     
 fi
 
 echo Done
-echo -e " ${BLUE} Tweet me @_nerdeveloper \e[0m"
+echo -e " ${BLUE} Tweet me @_nerdeveloper"
 
 
 

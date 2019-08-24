5ae81b2994dbb8e1f1845d6e8d1bdc5a42d25011
Lawrence Agbani <l.agbani@Lawrence.local>
2019-08-24 20:08:11 +0200

    Added missing sudo

--- installers/linux.sh
+++ installers/linux.sh
@@ -12,7 +12,7 @@ function install_nodejs {
     echo -e " ${BLUE} Installing Nodejs and npm "
     sleep 2
     curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION} -o nodesource_setup.sh && \
-    sudo chmod +x nodesource_setup.sh && ./nodesource_setup.sh && \
+    sudo chmod +x nodesource_setup.sh && sudo ./nodesource_setup.sh && \
     sudo apt-get install nodejs -y
     echo -e  " ${BLUE} Nodejs and npm has been installed! \e[0m"
 }
